342d1b84d24f7090b6806c0696e5cf9b64f61788
Dilshawn Sahi <em8654@wayne.edu>
2020-01-20 20:04:02 -0500

    Changing the greetings

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Simple C++ program to display "Hello World"
 // Header file for input output functions
 #include <iostream>
@@ -14,22 +13,3 @@ int main()
 
     return 0;
 }
-=======
-// Simple C++ program to display "Hello World" 
-// Author: Dilshawn Sahi <em8654@wayne.edu>
-
-// Header file for input output functions 
-#include<iostream>  
-  
-using namespace std; 
-  
-// main function - 
-// where the execution of program begins 
-int main() 
-{ 
-    // prints hello world 
-    cout<<"Hello World!"; 
-      
-    return 0; 
-} 
->>>>>>> master
